0360a60d60de4f20a663114762398a9cfdfdeadd
ZaraKh-new <zara.khudinyan@zemaenterprises.com>
2025-11-19 17:02:49 +0400

    Initial clean project setup with .gitignore

---
+++ codecept.conf.js
@@ -0,0 +1,57 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './tests/*.js',
+  output: './output',
+
+  helpers: {
+    WebDriver: {
+      url: process.env.BASE_URL || 'https://www.aljazeera.com',
+      browser: 'chrome',
+      smartWait: 5000,
+      waitForTimeout: 10000,
+      timeouts: {
+        script: 60000,
+        'page load': 30000,
+      },
+      desiredCapabilities: {
+        'goog:chromeOptions': {
+          args: [
+            '--disable-gpu',
+            '--no-sandbox',
+            '--window-size=1440,900',
+          ],
+        },
+      },
+    },
+  },
+
+  include: {
+    I: './support/steps_file.js',
+    HomePage: './pages/home.page.js',
+    LivePage: './pages/live.page.js',
+    MostPopularComponent: './pages/components/mostPopular.component.js',
+  },
+
+  gherkin: {
+    features: './features/**/*.feature',
+    steps: [
+      './step_definitions/mostRead.steps.js',
+      './step_definitions/liveStream.steps.js',
+    ],
+  },
+
+  plugins: {
+    screenshotOnFail: { enabled: true },
+    retryFailedStep: { enabled: true, retries: 1 },
+    pauseOnFail: {},
+    allure: {
+      enabled: true,
+      outputDir: 'output/allure-results',
+    },
+  },
+
+  name: 'pogos-aljazeera-e2e-codeceptjs',
+};
